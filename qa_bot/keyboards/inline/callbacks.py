8045d2571df2d9c567a055aaf97d59f618535479
karim <@1>
2023-10-22 16:19:21 +0300

    Add Answer and Reactions callbacks.

--- qa_bot/keyboards/inline/callbacks.py
+++ qa_bot/keyboards/inline/callbacks.py
@@ -1,5 +1,17 @@
 from aiogram.filters.callback_data import CallbackData
 
 
-class Action(CallbackData, prefix="act"):
-    action: str
+class AnswerCallback(CallbackData, prefix="answer"):
+    question_text: str
+    question_msg_id: int
+    support_chat_id: int
+    msg_id_to_edit: int
+    asker_id: int
+
+class ReactionCallback(CallbackData, prefix="reaction"):
+    is_help: bool
+    admin_chat_id: int
+    answer_msg_id: int
+    answer_msg_text: str
+    asker_id: int
+
