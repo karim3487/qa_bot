7010bc0795522aeadc5c1ab2e4e79fd9928a0b97
karim <@1>
2023-10-25 15:49:55 +0300

    Remove question_text & answer_text fields from Callbacks.

--- qa_bot/keyboards/inline/callbacks.py
+++ qa_bot/keyboards/inline/callbacks.py
@@ -5,7 +5,7 @@ from aiogram.filters.callback_data import CallbackData
 
 class AnswerCallback(CallbackData, prefix="answer"):
     # Text of the question
-    question_text: str
+    # question_text: str
     # Message ID of the question
     question_msg_id: int
     # Chat ID with support where the question originated
@@ -21,11 +21,7 @@ class ReactionCallback(CallbackData, prefix="reaction"):
     admin_chat_id: int
     # Message ID with the administrator's response
     answer_msg_id: Optional[int] = None
-    # Text of the message with the administrator's response
-    answer_msg_text: str
     # ID of the person who asked the question
     asker_id: int
-    # Text of the question
-    question_text: Optional[str] = None
     # Message ID of the question
     question_msg_id: Optional[int] = None
