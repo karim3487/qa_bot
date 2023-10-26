204be9d92fe2a0f8c414333d958548b5b6a6dd68
karim <@1>
2023-10-26 15:40:55 +0300

    Update add_answer handler.

--- qa_bot/handlers/admin_group/add_answer.py
+++ qa_bot/handlers/admin_group/add_answer.py
@@ -1,3 +1,5 @@
+import re
+
 from aiogram import types
 
 from qa_bot.web_handlers import api
@@ -8,7 +10,12 @@ async def add_new_answer(msg: types.Message):
         await msg.answer('Вы должны ответить на какое-нибудь сообщение этой командой, чтобы добавить ответ в БД.')
         return
 
+    regex_pattern = r'/ответить -\d+ \d+ .+'
     answer = msg.reply_to_message.text
+
+    if re.match(regex_pattern, answer):
+        answer = answer.split(' ', 3)[-1]
+
     res = api.add_answer(answer)
     # if res.status_code == 200:
     if res['status_code'] == 200:
