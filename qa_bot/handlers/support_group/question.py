e7c14a2d2d4c0e6d5557fe02da5738f600787879
karim <@1>
2023-11-22 15:57:03 +0300

    Call AutoResponder API

--- qa_bot/handlers/support_group/question.py
+++ qa_bot/handlers/support_group/question.py
@@ -4,7 +4,8 @@ from aiogram.fsm.context import FSMContext
 from qa_bot.data import config
 from qa_bot.keyboards.inline.answer import make_start_answer_keyboard
 from qa_bot.keyboards.inline.reactions import make_reaction_keyboard
-from qa_bot.web_handlers import api
+from qa_bot.utils.api.auto_responder_api import auto_responder_api
+from qa_bot.utils.enums import TypeOfMessages
 
 
 async def new_msg_in_group(msg: types.Message) -> None:
@@ -16,41 +17,59 @@ async def new_msg_in_group(msg: types.Message) -> None:
     admin_chat_id = config.ADMIN_CHAT_ID
     username_url = f'<a href="tg://user?id={msg.from_user.id}">{html.quote(msg.from_user.full_name)}</a>'
 
-    result = api.send_message_to_api(question_text)
-    if result == 1:
-        answer_text = "Ответ"
-        rkb = make_reaction_keyboard(admin_chat_id=admin_chat_id,
-                                     q_msg_id=question_msg_id)
+    message_type, result = await auto_responder_api.get_answer_to_question(
+        question_text
+    )
+    if message_type == TypeOfMessages.IS_Q_WITH_ANSWER:
+        answer_text = result
+        rkb = make_reaction_keyboard(
+            admin_chat_id=admin_chat_id, q_msg_id=question_msg_id
+        )
 
         response_message = [
-            'Ответ на Ваш вопрос:',
-            f'<code>{answer_text}</code>',
-            '\nПомог ли вам ответ?',
-            '👍 – Да',
-            '👎 – Нет',
+            "Ответ на Ваш вопрос:",
+            f"<code>{answer_text}</code>",
+            "\nПомог ли вам ответ?",
+            "👍 – Да",
+            "👎 – Нет",
         ]
 
-        await msg.reply(text='\n'.join(response_message),
-                        reply_markup=rkb)
+        await msg.reply(text="\n".join(response_message), reply_markup=rkb)
 
-    elif result == 2:
+    elif message_type == TypeOfMessages.IS_Q_WITHOUT_ANSWER:
         user_question_message = [
-            f'Пользователь {username_url} задал вопрос, ответ на который не нашелся в системе:',
-            f'<code>{html.quote(msg.text)}</code>',
+            f"Пользователь {username_url} задал вопрос, ответ на который не нашелся в системе:",
+            f"<code>{html.quote(msg.text)}</code>",
         ]
-        rkb = make_start_answer_keyboard(support_chat_id=msg.chat.id, q_msg_id=msg.message_id)
-        await msg.reply('Подождите немного, админ ответит на этот вопрос через некоторое время.')
-        await msg.bot.send_message(chat_id=admin_chat_id, text="\n".join(user_question_message), reply_markup=rkb)
+        rkb = make_start_answer_keyboard(
+            support_chat_id=msg.chat.id, q_msg_id=msg.message_id
+        )
+        await msg.reply(
+            "Подождите немного, админ ответит на этот вопрос через некоторое время."
+        )
+        await msg.bot.send_message(
+            chat_id=admin_chat_id,
+            text="\n".join(user_question_message),
+            reply_markup=rkb,
+        )
 
 
 async def last_question(msg: types.Message, state: FSMContext):
     admin_chat_id = config.ADMIN_CHAT_ID
     username_url = f'<a href="tg://user?id={msg.from_user.id}">{html.quote(msg.from_user.full_name)}</a>'
     user_last_question_message = [
-        f'Пользователю {username_url} не понравился ответ. Он задал еще один вопрос:',
-        f'<code>{html.quote(msg.text)}</code>',
+        f"Пользователю {username_url} не понравился ответ. Он задал еще один вопрос:",
+        f"<code>{html.quote(msg.text)}</code>",
     ]
-    rkb = make_start_answer_keyboard(support_chat_id=msg.chat.id, q_msg_id=msg.message_id)
-    await msg.reply('Подождите немного, админ ответит на этот вопрос через некоторое время.')
-    await msg.bot.send_message(chat_id=admin_chat_id, text="\n".join(user_last_question_message), reply_markup=rkb)
+    rkb = make_start_answer_keyboard(
+        support_chat_id=msg.chat.id, q_msg_id=msg.message_id
+    )
+    await msg.reply(
+        "Подождите немного, админ ответит на этот вопрос через некоторое время"
+    )
+    await msg.bot.send_message(
+        chat_id=admin_chat_id,
+        text="\n".join(user_last_question_message),
+        reply_markup=rkb,
+    )
     await state.clear()
